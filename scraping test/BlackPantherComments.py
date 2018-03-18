03e9fbf3812c31711718699bda35e6d6e1b495ce
gacima <31155177+gacima@users.noreply.github.com>
2018-03-18 16:44:01 +0800

    古墓丽影 评论分析完成
    done

--- scraping test/BlackPantherComments.py
+++ scraping test/BlackPantherComments.py
@@ -1,6 +1,10 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import string, re
+import string, re, jieba, numpy
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 
 def getFilmInfo(url):
@@ -43,6 +47,38 @@ def getComments(filmId, num):
     return comments
 
 
+def generateImage(file):
+    file = open('comments.txt', 'r', encoding='utf-8')
+    comments = file.read()
+
+    pattern = re.compile(r'[\u4e00-\u9fa5]+')
+    filterdata = re.findall(pattern, comments)
+    cleaned_comments = ''.join(filterdata)
+
+    segment = jieba.lcut(cleaned_comments)
+    words_df = pd.DataFrame({'segment': segment})
+
+    stopwords = pd.read_csv("chineseStopWords.txt", index_col=False, quoting=3, sep="\t", names=['stopword'],
+                            encoding='GBK')  # quoting=3全不引用
+    words_df = words_df[~words_df.segment.isin(stopwords.stopword)]
+    print(words_df.head())
+    words_stat = words_df.groupby(by=['segment'])['segment'].agg({"count": numpy.size})
+    words_stat = words_stat.reset_index().sort_values(by=["count"], ascending=False)
+
+    matplotlib.rcParams['figure.figsize'] = (10.0, 5.0)
+    # from wordcloud import WordCloud#词云包
+    wordcloud = WordCloud(font_path="simhei.ttf", background_color="white", max_font_size=80)  # 指定字体类型、字体大小和字体颜色
+    word_frequence = {x[0]: x[1] for x in words_stat.head(1000).values}
+    word_frequence_list = []
+    for key in word_frequence:
+        temp = (key, word_frequence[key])
+        word_frequence_list.append(temp)
+
+    wordcloud = wordcloud.fit_words(dict(word_frequence_list))
+    plt.imshow(wordcloud)
+    plt.savefig('comments.jpg')
+
+
 if __name__ == '__main__':
     commentsList = []
     filmId = getFilmInfo('https://movie.douban.com/cinema/nowplaying/chengdu/')['id']
@@ -53,12 +89,9 @@ if __name__ == '__main__':
     comments = ''
     for a in commentsList:
         comments += str(a).strip(string.punctuation)
-    #file = open('comments.txt', 'w', encoding='utf-8')
-    #file.write(comments)
     pattern = re.compile(r'[\u4e00-\u9fa5]+')
     comments = re.findall(pattern, comments)
     comments = ''.join(comments)
-
-#getFilmInfo('https://movie.douban.com/cinema/nowplaying/chengdu/')
-def cleanData():
-    file = open('comments.txt')
+    file = open('comments.txt', 'w', encoding='utf-8')
+    file.write(comments)
+    generateImage(file)
